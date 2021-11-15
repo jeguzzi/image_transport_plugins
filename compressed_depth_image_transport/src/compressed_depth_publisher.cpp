ec14f3c8542d30065ac2d5f6bdcbf3a2852095b1
Jerome Guzzi <jerome@idsia.ch>
2021-11-15 13:22:18 +0100

    Update compressed_depth_publisher.cpp

--- compressed_depth_image_transport/src/compressed_depth_publisher.cpp
+++ compressed_depth_image_transport/src/compressed_depth_publisher.cpp
@@ -64,12 +64,16 @@ void CompressedDepthPublisher::advertiseImpl(
 {
   typedef image_transport::SimplePublisherPlugin<sensor_msgs::msg::CompressedImage> Base;
   Base::advertiseImpl(node, base_topic, custom_qos);
-  node->declare_parameter("png_level", kDefaultPngLevel);
-  node->declare_parameter("depth_max", kDefaultDepthMax);
-  node->declare_parameter("depth_quantization", KDefaultDepthQuantization);
-  node->get_parameter_or<int>("png_level", config_.png_level, kDefaultPngLevel);
-  node->get_parameter_or<double>("depth_max", config_.depth_max, kDefaultDepthMax);
-  node->get_parameter_or<double>("depth_quantization", config_.depth_max, KDefaultDepthQuantization);
+  uint ns_len = node->get_effective_namespace().length();
+  std::string param_base_name = base_topic.substr(ns_len);
+  std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
+
+  node->declare_parameter(param_base_name+".png_level", kDefaultPngLevel);
+  node->declare_parameter(param_base_name+".depth_max", kDefaultDepthMax);
+  node->declare_parameter(param_base_name+".depth_quantization", KDefaultDepthQuantization);
+  node->get_parameter_or<int>(param_base_name+".png_level", config_.png_level, kDefaultPngLevel);
+  node->get_parameter_or<double>(param_base_name+".depth_max", config_.depth_max, kDefaultDepthMax);
+  node->get_parameter_or<double>(param_base_name+".depth_quantization", config_.depth_max, KDefaultDepthQuantization);
 }
 
 void CompressedDepthPublisher::publish(
